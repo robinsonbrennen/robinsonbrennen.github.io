d2e731f4acba9a3254360ca3386245c32fabc64d
robinsonbrennen <rob16073@byui.edu>
2020-10-01 20:15:48 -0700

    c3

--- assignmentwebsite/date.js
+++ assignmentwebsite/date.js
@@ -26,7 +26,7 @@ let months = [
 
 let d = new Date();
 let dayName = daynames[d.getDay()];
-let monthName = monthnames[d.getMonth()];
+let monthName = months[d.getMonth()];
 let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
 
 document.getElementById("currentDate").innerHTML = fulldate;
